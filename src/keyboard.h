633d1f3904d74bc3103433835b9c250305b6bffa
Ayden Paul Bottos <aydenbottos12@gmail.com>
2025-05-30 23:15:41 +1000

    Add scancode function declarations

--- src/keyboard.h
+++ src/keyboard.h
@@ -2,18 +2,22 @@
 #define KEYBOARD_H
 
 #include <stdbool.h>
-#include "interrupts.h"  // for struct regs
+#include <stdint.h>
+#include "interrupts.h"
 
-// Called by the IRQ stub on vector 0x21
-void keyboard_handler(struct regs *r);
+// Special scan codes
+#define SCANCODE_UP    0x48
+#define SCANCODE_DOWN  0x50
+#define SCANCODE_LEFT  0x4B
+#define SCANCODE_RIGHT 0x4D
+#define SCANCODE_SPACE 0x39
+#define SCANCODE_ESC   0x01
 
-// Initialize keyboard
 void keyboard_init(void);
-
-// Check if keyboard input is available
+void keyboard_handler(regs_t* regs);
 bool keyboard_has_input(void);
-
-// Get next character from keyboard buffer
 char keyboard_get_char(void);
+bool keyboard_has_scancode(void);
+uint8_t keyboard_get_scancode(void);
 
-#endif /* KEYBOARD_H */
+#endif // KEYBOARD_H
